40983139deda29cc8f87adc90b4f3b48127ca36b
Vijay Patidar <patidarvijay590@gmail.com>
2023-03-28 17:06:23 +0530

    responsive web page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "session#destroy"
-
+  get "/demo", to: "users#demo"
   # get 'users/new'
   root "static_pages#home"
   # get 'static_pages/contact'
