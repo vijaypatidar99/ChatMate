267d100998995f2f40e7b14055ee24bb711075cc
Vijay Patidar <patidarvijay590@gmail.com>
2023-03-29 19:13:45 +0530

    test--

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,6 +29,7 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.encrypted[:user_id])
+      raise
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
-require 'minitest/reporters'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
+require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
@@ -16,5 +16,19 @@ class ActiveSupport::TestCase
   # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Log in as a particular user.
+  def log_in_as(user, password: "password", remember_me: "1")
+    post login_path, params: { session: { email: user.email,
+                                         password: password,
+                                         remember_me: remember_me } }
   end
 end
