21b74f1370bee0027958e5ad2d49386300576b68
Vijay Patidar <patidarvijay590@gmail.com>
2023-03-29 10:52:42 +0530

    modified delete method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,8 +14,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete(:user_id)
-    @current_user = nil
-    redirect_to root_url
+    log_out
+    # session.delete(:user_id)
+    # @current_user = nil
+    redirect_to root_path, notice: "You have been logged out."
   end
 end
