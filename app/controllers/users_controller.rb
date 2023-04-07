9280324bf20db44b0d81ecbd39ef8066bee23a56
Vijay Patidar <patidarvijay590@gmail.com>
2023-04-07 13:49:00 +0530

    followers/following tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,14 +70,6 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  # def logged_in_user
-  #   unless logged_in?
-  #     store_location
-  #     flash[:danger] = "Please log in."
-  #     redirect_to login_url
-  #   end
-  # end
-
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
